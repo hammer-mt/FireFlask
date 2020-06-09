866baa38d7f027ab3da00d2b9345487842952a5b
Hammer <=>
2020-06-09 12:28:29 +0100

    added forms to the application

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 from app import app
 
 @app.route('/')
@@ -16,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
